27ab9fb6a9ea3f25815ec33003c81bb102324774
kavehkaveh1 <kaveh.nassiri1998@gmail.com>
2025-04-11 16:43:21 +0330

    writing simple personal detail

--- App.jsx
+++ App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 function App() {
-  return <div>App</div>;
+  return;
+  <div>
+    <form action="">
+      <fieldset>
+        <legend>personal data</legend>
+        <p>
+          <label htmlFor="firstName"></label>
+          <input type="text" name="firstName" id="firstName" />
+        </p>
+        <p>
+          <label htmlFor="lastName"></label>
+          <input type="text" name="lastName" id="lastName" />
+        </p>
+      </fieldset>
+    </form>
+  </div>;
 }
 
 export default App;
