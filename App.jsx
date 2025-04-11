54908013e49281bf53b797b74b1f42a77c0d0a92
kavehkaveh1 <kaveh.nassiri1998@gmail.com>
2025-04-11 18:08:30 +0330

    submit button

--- App.jsx
+++ App.jsx
@@ -3,118 +3,131 @@ import React from "react";
 function App() {
   return (
     <div>
-      <form action="">
+      <form>
         <fieldset>
-          <legend>personal data (required)</legend>
-          <p>
-            <label htmlFor="firstName">firstName : </label>
-            <input
-              type="text"
-              name="firstName"
-              id="firstName"
-              placeholder="jone"
-              required
-              autoFocus
-            />
-          </p>
-          <p>
-            <label htmlFor="lastName">lastName : </label>
-            <input
-              type="text"
-              name="lastName"
-              id="lastName"
-              placeholder="doe"
-              required
-            />
-          </p>
-          <p>
-            <label htmlFor="password">password : </label>
-            <input type="password" name="password" id="password" required />
-          </p>
-          <p>
-            <label htmlFor="phoneNumber">phoneNumber : </label>
-            <input
-              type="tel"
-              name="phoneNumber"
-              id="phoneNumber"
-              required
-              pattern="[0-9]{11}"
-            />
-          </p>
-          <p>
-            <label htmlFor="birthDate">birthDate : </label>
-            <input type="date" name="date" id="birthDate" required />
-          </p>
-          <p>
-            <label htmlFor="email">email : </label>
-            <input
-              type="email"
-              neme="email"
-              id="email"
-              required
-              placeholder="....@gmail.com"
-            />
-          </p>
-        </fieldset>
-        <fieldset>
-          <legend>personal data (optional)</legend>
-          <p>
-            <label htmlFor="number">your lucky number : </label>
-            <input type="number" name="number" id="number" max={100} min={0} />
-          </p>
-          <p>
-            <label htmlFor="drinks"> favorite drinks : </label>
-            <select name="drink" id="drinks">
-              <optgroup label="cold drinks">
-                <option value="iceTea">iceTea</option>
-                <option value="orangeJuice">orangeJuice</option>
-                <option value="others">others</option>
-              </optgroup>
-              <optgroup label="hot drinks">
-                <option value="tea">tea</option>
-                <option value="coffee">coffee</option>
-                <option value="others">others</option>
-              </optgroup>
-            </select>
-          </p>
-          <div>
-            <p>choose your pet : </p>
+          <legend> info : </legend>
+          <fieldset>
+            <legend>personal data (required)</legend>
             <p>
-              <input type="checkbox" name="pets" id="dog" />
-              <label htmlFor="dog">dog</label>
+              <label htmlFor="firstName">firstName : </label>
+              <input
+                type="text"
+                name="firstName"
+                id="firstName"
+                placeholder="jone"
+                required
+                autoFocus
+              />
             </p>
             <p>
-              <input type="checkbox" name="pets" id="cat" />
-              <label htmlFor="cat">cat</label>
+              <label htmlFor="lastName">lastName : </label>
+              <input
+                type="text"
+                name="lastName"
+                id="lastName"
+                placeholder="doe"
+                required
+              />
             </p>
             <p>
-              <input type="checkbox" name="pets" id="fish" />
-              <label htmlFor="fish">fish</label>
+              <label htmlFor="password">password : </label>
+              <input type="password" name="password" id="password" required />
             </p>
             <p>
-              <input type="checkbox" name="pets" id="others" />
-              <label htmlFor="others">others</label>
-            </p>
-          </div>
-          <div>
-            <p>choose your favorite food : </p>
-            <p>
-              <input type="radio" name="food" id="pizza" />
-              <label htmlFor="pizza">pizza</label>
-            </p>
-            <p>
-              <input type="radio" name="food" id="kabab" />
-              <label htmlFor="kabab">kabab</label>
+              <label htmlFor="phoneNumber">phoneNumber : </label>
+              <input
+                type="tel"
+                name="phoneNumber"
+                id="phoneNumber"
+                required
+                pattern="[0-9]{11}"
+              />
             </p>
             <p>
-              <input type="radio" name="food" id="khoresh" />
-              <label htmlFor="khoresh">khoresh</label>
+              <label htmlFor="birthDate">birthDate : </label>
+              <input type="date" name="date" id="birthDate" required />
             </p>
             <p>
-              <input type="radio" name="food" id="others" />
-              <label htmlFor="others">others</label>
+              <label htmlFor="email">email : </label>
+              <input
+                type="email"
+                neme="email"
+                id="email"
+                required
+                placeholder="....@gmail.com"
+              />
             </p>
-          </div>
+          </fieldset>
+          <fieldset>
+            <legend>personal data (optional)</legend>
+            <p>
+              <label htmlFor="number">your lucky number : </label>
+              <input
+                type="number"
+                name="number"
+                id="number"
+                max={100}
+                min={0}
+              />
+            </p>
+            <p>
+              <label htmlFor="drinks"> favorite drinks : </label>
+              <select name="drink" id="drinks">
+                <optgroup label="cold drinks">
+                  <option value="iceTea">iceTea</option>
+                  <option value="orangeJuice">orangeJuice</option>
+                  <option value="others">others</option>
+                </optgroup>
+                <optgroup label="hot drinks">
+                  <option value="tea">tea</option>
+                  <option value="coffee">coffee</option>
+                  <option value="others">others</option>
+                </optgroup>
+              </select>
+            </p>
+            <div>
+              <p>choose your pet : </p>
+              <p>
+                <input type="checkbox" name="pets" id="dog" />
+                <label htmlFor="dog">dog</label>
+              </p>
+              <p>
+                <input type="checkbox" name="pets" id="cat" />
+                <label htmlFor="cat">cat</label>
+              </p>
+              <p>
+                <input type="checkbox" name="pets" id="fish" />
+                <label htmlFor="fish">fish</label>
+              </p>
+              <p>
+                <input type="checkbox" name="pets" id="others" />
+                <label htmlFor="others">others</label>
+              </p>
+            </div>
+            <div>
+              <p>choose your favorite food : </p>
+              <p>
+                <input type="radio" name="food" id="pizza" />
+                <label htmlFor="pizza">pizza</label>
+              </p>
+              <p>
+                <input type="radio" name="food" id="kabab" />
+                <label htmlFor="kabab">kabab</label>
+              </p>
+              <p>
+                <input type="radio" name="food" id="khoresh" />
+                <label htmlFor="khoresh">khoresh</label>
+              </p>
+              <p>
+                <input type="radio" name="food" id="others" />
+                <label htmlFor="others">others</label>
+              </p>
+            </div>
+          </fieldset>
+          <p>
+            <button type="submit"> submit</button>{" "}
+            <button type="reset">reset</button>
+          </p>
         </fieldset>
       </form>
     </div>
