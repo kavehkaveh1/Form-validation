c9d9004fb7bfed0db33d5005881e258761ccf97b
kavehkaveh1 <kaveh.nassiri1998@gmail.com>
2025-04-13 18:09:08 +0330

    correct the form

--- App.jsx
+++ App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div>
-      <form>
+      <form action="/">
         <fieldset>
           <legend> info : </legend>
           <fieldset>
