bae00dc1be97161111fbe40ec7c8a2cfa393ce07
kavehkaveh1 <kaveh.nassiri1998@gmail.com>
2025-04-14 18:02:54 +0330

    make the errer massage hide and some other stuf

--- formValidation/component/inputForm.jsx
+++ formValidation/component/inputForm.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./inputFormStyle.css";
 function InputForm(props) {
   const { onChange, errorMassage, lable, ...others } = props;
+  const [focused, setFocused] = useState(false);
+  const handleChange = (e) => {
+    setFocused(true);
+  };
   return (
     <div className="formInput">
       <label>{lable} : </label>
-      <input onChange={onChange} {...others} />
+      <input
+        onChange={onChange}
+        {...others}
+        onBlur={handleChange}
+        focused={focused.toString()}
+      />
       <span>{errorMassage}</span>
     </div>
   );
