f8e960d199858693d99f9620e2720a3f6449c979
kavehkaveh1 <kaveh.nassiri1998@gmail.com>
2025-04-15 16:50:27 +0330

    adding register and handleSubmit and formState to the form

--- hookForm/App.jsx
+++ hookForm/App.jsx
@@ -1,20 +1,43 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div>
-      <form action="/">
-        <p>
+      <form action="/" onSubmit={handleSubmit(onSubmit)}>
+        <div>
           <label htmlFor="userName">userName : </label>
-          <input type="text" id="userName" />
-        </p>
-        <p>
+          <input
+            type="text"
+            {...register("userName", {
+              required: "username is required",
+            })}
+            id="userName"
+          />
+          {errors.userName && <div> {errors.userName.message} </div>}
+        </div>
+        <div>
           <label htmlFor="password">passwordf : </label>
-          <input type="password" id="password" />
-        </p>
-        <p>
+          <input
+            type="password"
+            {...register("password", {
+              required: "password is required",
+            })}
+            id="password"
+          />
+          {errors.password && <div> {errors.password.message} </div>}
+        </div>
+        <div>
           <button type="submit">submit</button>
-        </p>
+        </div>
       </form>
     </div>
   );
